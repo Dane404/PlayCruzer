16ea3e6985331145b302200f1eeef4fc56857303
Danijel Pejic <danijelpejic15@gmail.com>
2021-05-24 20:53:08 +0200

    fixed searchbar input( onenterpress event)

--- WebPart/eventFunctions.js
+++ WebPart/eventFunctions.js
@@ -2,13 +2,24 @@ async function On_Click(event)
 {
     window.location.replace("./gamePage.html?id="+event.target.alt);
  }
-function On_Submit()
+function On_Submit(event)
 {
+    if(event.keyCode==13)
+    {
+        let searchbar = document.getElementById("searchBar");
+        let searchString = slugify(searchbar.value);
     
+        window.location.replace("./searchSite.html?search="+searchString+"&page=1");
+    }
+    
+}
+function On_ButtonClick()
+{
     let searchbar = document.getElementById("searchBar");
     let searchString = slugify(searchbar.value);
-
     window.location.replace("./searchSite.html?search="+searchString+"&page=1");
+
+
 }
 function slugify(stringToSlug)
 {
