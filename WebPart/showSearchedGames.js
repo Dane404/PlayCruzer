2b03f65bf10052142b535193d14c0626b58ab185
Danijel Pejic <danijelpejic15@gmail.com>
2021-06-08 07:43:23 +0200

    formated searchsite( ich will nicht mehr)

--- WebPart/showSearchedGames.js
+++ WebPart/showSearchedGames.js
@@ -13,26 +13,49 @@ request.onload= function()
         //create games to be shown
         for(let i = 0;i<response.results.length;i++)
         {
+
             let formatGameItem = document.createElement("div");// div to format games
             formatGameItem.setAttribute("class","col-md-6 col-lg-4 mb-5");
             let gameItem = document.createElement("div");// the game item div
             gameItem.setAttribute("class","game-item mx-auto");
-            let gameTitle = document.createElement("p"); //the game title 
-            gameTitle.innerText=response.results[i].name;
+
+            let cardDiv = document.createElement("div");
+            cardDiv.setAttribute("class","card");
+            cardDiv.setAttribute("style","18rem");
+
+            let cardBody = document.createElement("div");
+            cardBody.setAttribute("class","card-img-top");
             
+            
+            let gameTitle = document.createElement("h5"); //the game title 
+            gameTitle.innerText=response.results[i].name;
+            gameTitle.setAttribute("class","card-text");
+
+
             let createdLink = document.createElement("a"); // link to get on the game site
-           
             createdLink.setAttribute("onclick","On_Click(event)");
+
             let createdImage = document.createElement("img");//the image to be shown
+            createdImage.setAttribute("class","card-img-bottom");
+            createdImage.setAttribute("background-size","auto");
+            
+  
             createdImage.alt = response.results[i].id;
-            createdImage.setAttribute("object-fit","fill");
             createdImage.style.width="500px";
             createdImage.style.height="100px";
-            createdImage.setAttribute("height","150px");
+            if(response.results[i].background_image===null)
+            {
+                createdImage.src = "assets/img/Logo.png";
+            }
+            else{
+                createdImage.src = response.results[i].background_image;
+            }
+            cardBody.appendChild(gameTitle);
+           
+            cardBody.appendChild(createdImage);
             
-            createdImage.src = response.results[i].background_image;
-            createdLink.append(createdImage);
-            gameItem.appendChild(gameTitle);
+            cardDiv.appendChild(cardBody);
+            createdLink.appendChild(cardDiv);
             gameItem.appendChild(createdLink);
             formatGameItem.appendChild(gameItem);
             document.getElementById("foundGames").appendChild(formatGameItem);
